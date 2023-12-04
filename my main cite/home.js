6189cc64d3ad6fdaa102e9b43a38ccd4e6f9a4f8
Mohammadrezaheidariezzati <mohammadrezaheidariezzati7408@gmail.com>
2023-12-04 13:08:05 +0330

    edit

--- my main cite/home.js
+++ my main cite/home.js
@@ -4,9 +4,10 @@ const container = document.querySelector(".container");
 hamburger_menu.addEventListener("click",()=>{
     container.classList.toggle("active");
 });
-
+/*
 const btn = document.querySelector(".btn");
 
 btn.addEventListener("click",()=>{
     container.classList.toggle("active");
 });
+*/
