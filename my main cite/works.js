4c136ec2435d57bbc61a5c683f97390172c35bd0
Mohammadrezaheidariezzati <mohammadrezaheidariezzati7408@gmail.com>
2024-04-13 03:52:19 +0330

    work edition

--- my main cite/works.js
+++ my main cite/works.js
@@ -2,10 +2,11 @@ let next = document.querySelector('.next')
 let prev = document.querySelector('.prev')
 
 next.addEventListener('click',function(){
-    let item = document.querySelector('.item')
+    let items = document.querySelector('.item')
     document.querySelector('.slide').appendChild(items[0]) 
 })
+
 prev.addEventListener('click',function(){
-    let item = document.querySelector('.item')
+    let items = document.querySelector('.item')
     document.querySelector('.slide').prepend(items[items.length -1]) 
 })
